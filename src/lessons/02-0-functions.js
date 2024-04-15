a2074c630237d97f471785aaa986d265ac0e743a
Alex Bilba <alex.shanless@gmail.com>
2024-04-15 11:13:42 -0700

    Update 02-0-functions.js

--- src/lessons/02-0-functions.js
+++ src/lessons/02-0-functions.js
@@ -5,13 +5,21 @@
 // Define a function named 'greet' that takes one argument 'name'.
 // The function should console.log a greeting string: "Hello, [name]!"
 
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Call the function with the name "Bob"
+
 // Define a function that squares a given number
 // Bind the function to a const variable named square
-
+const square = function (num) {
+  return num * num;
+};
 // Define a binding named cubed whose value is a function with one parameter named num.
 // The function should return the value of num cubed.
 // The function should be defined using ES6 fat arrow syntax
-
+const cubed = num => num * num * num;
 /**
  * Task 2: Bindings and Scopes
  */
@@ -21,26 +29,31 @@ function scopedFunction() {
   let b = 2;
   var c = 3;
   // modify the statement below to the value that scopedFunction returns true
-  return a + b + c === 99;
+  return a + b + c === 6;
 }
 const result = scopedFunction();
 
 let d = 4;
 // check that 'a' exists in the global scope, if it does, set d equal to 5
-
+if (a) {
+  d = 5;
+}
 /**
  * Task 3: Bindings and Scopes (same variable name)
  */
 
 // Define a function named halve that takes one parameter named num.
 // The function should return num halved.
+const halve = function (num) {
+  return num / 2;
+};
 
 // Declare a variable named num in global scope and assign it the value 100.
-
+const num = 100;
 // create a variable named halfOfFifty and set it to the return value of halve(50)
-
+const halfOfFifty = halve(50);
 // create a variable named halfOfNum and set it to the return value of halve(num)
-
+const halfOfNum = halve(num);
 /**
  * Task 4: Complete the breadRecipe function
  * Objective: Understand nested functions and scopes
@@ -49,19 +62,31 @@ let d = 4;
 // Create a function named breadRecipe that takes one parameter named numLoaves
 const breadRecipe = function (numLoaves) {
   // Create a function named addIngredient
+  const addIngredient = function (amount, unit, ingredient) {
+    const ingredientAmount = amount * numLoaves;
+    if (ingredientAmount > 1) {
+      unit = `${unit}s`;
+    }
+    console.log(`${ingredientAmount} ${unit} ${ingredient}`);
+  };
   // The function should take three parameters: amount, unit, ingredient
   // Calculate total required ingredient amount based on the number of loaves
   // The function should log the ingredient amount and name
   // The function should modify the unit to plural if ingredientAmount is greater than 1
   // example: 1 cup flour
   // example: 2 cups water
+
   // Add ingredients using the addIngredient function
   // the recipe for one loaf is: 2 cups flour,  2 cups water, 1 teaspoon salt, 0.5 teaspoon yeast
   // example. addIngredient(2, "cup", "flour");
+  addIngredient(2, 'cup', 'flour');
+  addIngredient(2, 'cup', 'water');
+  addIngredient(1, 'teaspoon', 'salt');
+  addIngredient(0.5, 'teaspoon', 'yeast');
 };
 
 // Call the breadRecipe function with 2 loaves
-
+breadRecipe(2);
 /**
  * Task 5: Optional Parameters
  *
@@ -73,7 +98,9 @@ const breadRecipe = function (numLoaves) {
 // if num2 is not defined, set the default value to 0
 // example: minus(2, 5) should return 3
 // example: minus(5) should return -5
-
+const minus = function (num1, num2 = 0) {
+  return num2 - num1;
+};
 /**
  * Task 6: Rest Parameters and Spread Operator
  * Objective: Understand the rest parameter syntax
@@ -83,17 +110,13 @@ const breadRecipe = function (numLoaves) {
 // the function should take a rest parameter named nums
 // the function should return the sum of all numbers passed in
 // call the function with 1, 2, 3, 4, 5
-const sumRest = function (...nums) {
-
-};
+const sumRest = function (...nums) {};
 
 // create a function named sumSpread
 // the function should take one parameter name nums that is an array
 // Use the spread operator to calculate the sum of all numbers in the array
 // It should use the sumRest function defined above
-const sumSpread = function (nums) {
-
-};
+const sumSpread = function (nums) {};
 
 /**
  * Task 7: Closure (idGenerator)
